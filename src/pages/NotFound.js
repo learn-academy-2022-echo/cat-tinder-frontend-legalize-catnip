c915063d09d2364151aa79aa881c6ab41cd734fa
Tuan Le <tuanz_z@yahoo.com>
2022-09-21 11:45:31 -0700

    all 4 tests passed

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -3,8 +3,7 @@ import React from "react";
 const NotFound = () => {
     return (
         <h1 className="Four" >
-            <img src="/error_cat.png" alt="404" />
-            404 thats an error. </h1>
+            <img src="/error_cat.png" alt="404" />404 thats an error.</h1>
     )
 }
 
