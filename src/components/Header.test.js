40dfb69f152c915de0a75f522c2595779c98a80a
Tuan Le <tuanz_z@yahoo.com>
2022-09-21 11:02:40 -0700

    header and home tests complete

---
+++ src/components/Header.test.js
@@ -0,0 +1,14 @@
+import { screen, render } from '@testing-library/react';
+import Header from './Header';
+import userEvent from '@testing-library/user-event';
+import { BrowserRouter } from 'react-router-dom';
+
+describe("<Header/>", () => {
+    test("header renders without error", () => {
+        render(<Header />)
+        const indexLink = screen.getByText(/Cat Tinder/i)
+        expect(indexLink).toBeInTheDocument()
+
+
+    })
+})
